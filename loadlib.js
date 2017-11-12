69474f8a5516251a26a4d261ddc8a839994c1663
erinsasha <erinsashavmk@gmail.com>
2017-11-12 19:46:30 +0300

    loadlib.js

---
+++ loadlib.js
@@ -0,0 +1,21 @@
+/**
+ * Created by erinsasha on 12/11/17.
+ */
+function er_loadlib() {
+    console.log('START er_loadlib');
+
+    var version = '6-2-8';
+    script=document.createElement('script');
+    script.type='text/javascript';
+    script.src='https://erinsasha.github.io/lhlib/lhlib-full.js?v='+version;
+    document.head.appendChild(script);
+
+    var style=document.createElement('link');
+    style.type='text/css';
+    style.rel='stylesheet';
+    style.href='https://erinsasha.github.io/lhlib/lhs.css?v='+version;
+    document.head.appendChild(style);
+
+    console.log('END er_loadlib');
+}
+er_loadlib();
