ee4366da9cd18629f9e37f457f57503eefc12cf5
erinsasha <erinsashavmk@gmail.com>
2017-11-15 12:56:10 +0300

    6-3-1 backup automatically

--- lhlib.js
+++ lhlib.js
@@ -4,7 +4,7 @@
 function er_loadlib() {
     console.log('START er_loadlib');
 
-    var version = '6-2-9';
+    var version = '6-3-1';
     script=document.createElement('script');
     script.type='text/javascript';
     script.src='https://erinsasha.github.io/lhlib/lhlib-full.js?v='+version;
