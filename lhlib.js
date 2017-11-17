526e4fdfb7181c7439cf38c20568a562ecf5353d
erinsasha <erinsashavmk@gmail.com>
2017-11-18 01:14:48 +0300

    6-3-9 my invites canceller - preload page

--- lhlib.js
+++ lhlib.js
@@ -4,7 +4,7 @@
 function er_loadlib() {
     console.log('START er_loadlib');
 
-    var version = '6-3-8';
+    var version = '6-3-9';
     script=document.createElement('script');
     script.type='text/javascript';
     script.src='https://erinsasha.github.io/lhlib/lhlib-full.js?v='+version;
