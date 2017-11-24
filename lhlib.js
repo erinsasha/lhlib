6614a55ff4ac8278ad726a8b4f0cda29378ab6b0
erinsasha <erinsashavmk@gmail.com>
2017-11-24 12:40:34 +0300

    6-5-1 group inviter fix

--- lhlib.js
+++ lhlib.js
@@ -4,7 +4,7 @@
 function er_loadlib() {
     console.log('START er_loadlib');
 
-    var version = '6-4-9';
+    var version = '6-5-1';
     script=document.createElement('script');
     script.type='text/javascript';
     script.src='https://erinsasha.github.io/lhlib/lhlib-full.js?v='+version;
