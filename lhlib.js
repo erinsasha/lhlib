5f93d91a3aede481d7a3dc94b0f21eb0f1944da6
erinsasha <erinsashavmk@gmail.com>
2017-12-18 10:51:18 +0300

    6-7-4 close other message popup when broadcast

--- lhlib.js
+++ lhlib.js
@@ -4,7 +4,7 @@
 function er_loadlib() {
     console.log('START er_loadlib');
 
-    var version = '6-7-3';
+    var version = '6-7-4';
     script=document.createElement('script');
     script.type='text/javascript';
     script.src='https://erinsasha.github.io/lhlib/lhlib-full.js?v='+version;
