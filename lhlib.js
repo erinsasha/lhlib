9946dcf8e9505b4c1f3878abec3214eeb2cdc505
erinsasha <erinsashavmk@gmail.com>
2018-04-06 19:19:38 +0300

    7-9-1 skip LinkedIn member

--- lhlib.js
+++ lhlib.js
@@ -4,7 +4,7 @@
 function er_loadlib() {
     console.log('START er_loadlib');
 
-    var version = '7-8-9';
+    var version = '7-9-1';
     script=document.createElement('script');
     script.type='text/javascript';
     script.src='https://erinsasha.github.io/lhlib/lhlib-full.js?v='+version;
