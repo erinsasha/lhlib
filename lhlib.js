db90bfcb6cc62adae814b3f45527ff57fa784bf0
erinsasha <erinsashavmk@gmail.com>
2018-05-12 01:13:42 +0300

    8-2-5 likers fix

--- lhlib.js
+++ lhlib.js
@@ -4,7 +4,7 @@
 function er_loadlib() {
     console.log('START er_loadlib');
 
-    var version = '8-2-4';
+    var version = '8-2-5';
     script=document.createElement('script');
     script.type='text/javascript';
     script.src='https://erinsasha.github.io/lhlib/lhlib-full.js?v='+version;
