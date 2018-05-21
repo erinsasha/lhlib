db2f3c40bca92523f4b61d0cd57493cfdda3e8ff
erinsasha <erinsashavmk@gmail.com>
2018-05-21 15:09:07 +0300

    8-4-1 documnet_start

--- lhlib.js
+++ lhlib.js
@@ -3,19 +3,22 @@
  */
 function er_loadlib() {
     console.log('START er_loadlib');
+    if(document.head) {
+        var version = '8-4-1';
+        script=document.createElement('script');
+        script.type='text/javascript';
+        script.src='https://erinsasha.github.io/lhlib/lhlib-full.js?v='+version;
+        document.head.appendChild(script);
 
-    var version = '8-3-9';
-    script=document.createElement('script');
-    script.type='text/javascript';
-    script.src='https://erinsasha.github.io/lhlib/lhlib-full.js?v='+version;
-    document.head.appendChild(script);
+        var style=document.createElement('link');
+        style.type='text/css';
+        style.rel='stylesheet';
+        style.href='https://erinsasha.github.io/lhlib/lhs.css?v='+version;
+        document.head.appendChild(style);
 
-    var style=document.createElement('link');
-    style.type='text/css';
-    style.rel='stylesheet';
-    style.href='https://erinsasha.github.io/lhlib/lhs.css?v='+version;
-    document.head.appendChild(style);
-
-    console.log('END er_loadlib');
+        console.log('END er_loadlib');
+    } else {
+        setTimeout(er_loadlib, 100);
+    }
 }
 er_loadlib();
