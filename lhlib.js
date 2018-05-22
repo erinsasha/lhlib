269f5ea25564ff3afd6f1f99595dcf9d233b7fcc
erinsasha <erinsashavmk@gmail.com>
2018-05-22 23:01:01 +0300

    8-4-9 recent collect fix

--- lhlib.js
+++ lhlib.js
@@ -4,7 +4,7 @@
 function er_loadlib() {
     console.log('START er_loadlib');
     if(document.head) {
-        var version = '8-4-8';
+        var version = '8-4-9';
         script=document.createElement('script');
         script.type='text/javascript';
         script.src='https://erinsasha.github.io/lhlib/lhlib-full.js?v='+version;
