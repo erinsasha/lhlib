ab5470a413cf749c2c59910c577430dc917cd7ec
erinsasha <erinsashavmk@gmail.com>
2018-06-12 15:02:01 +0300

    8-6-6 new sn search

--- lhlib.js
+++ lhlib.js
@@ -4,7 +4,7 @@
 function er_loadlib() {
     console.log('START er_loadlib');
     if(document.head) {
-        var version = '8-6-5';
+        var version = '8-6-6';
         script=document.createElement('script');
         script.type='text/javascript';
         script.src='https://erinsasha.github.io/lhlib/lhlib-full.js?v='+version;
