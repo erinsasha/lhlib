f476d218e8dbdc35ffff21781536d7ccaaefaf16
erinsasha <erinsashavmk@gmail.com>
2018-06-21 16:19:05 +0300

    8-8-3 PE skip vary LM

--- lhlib.js
+++ lhlib.js
@@ -4,7 +4,7 @@
 function er_loadlib() {
     console.log('START er_loadlib');
     if(document.head) {
-        var version = '8-8-2';
+        var version = '8-8-3';
         script=document.createElement('script');
         script.type='text/javascript';
         script.src='https://erinsasha.github.io/lhlib/lhlib-full.js?v='+version;
