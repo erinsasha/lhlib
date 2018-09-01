975a8bff649a6fefcf486e1bf3dc7f3fd15f8550
erinsasha <erinsashavmk@gmail.com>
2018-09-01 20:47:39 +0300

    9-5-7 follow sn fix

--- lhlib.js
+++ lhlib.js
@@ -4,7 +4,7 @@
 function er_loadlib() {
     console.log('START er_loadlib');
     if(document.head) {
-        var version = '9-5-6';
+        var version = '9-5-7';
         script=document.createElement('script');
         script.type='text/javascript';
         script.src='https://erinsasha.github.io/lhlib/lhlib-full.js?v='+version;
