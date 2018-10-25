806fee9ac02bf2d9ee397fa4d8c04b4efa8f147a
erinsasha <erinsashavmk@gmail.com>
2018-10-25 22:07:31 +0300

    10-3-3 comp extractor

--- lhlib.js
+++ lhlib.js
@@ -4,7 +4,7 @@
 function er_loadlib() {
     console.log('START er_loadlib');
     if(document.head) {
-        var version = '10-3-2';
+        var version = '10-3-3';
         script=document.createElement('script');
         script.type='text/javascript';
         script.src='https://erinsasha.github.io/lhlib/lhlib-full.js?v='+version;
