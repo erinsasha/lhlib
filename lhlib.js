091fa63dd5599e13979a9c8c45cdfe636cea4676
erinsasha <erinsashavmk@gmail.com>
2018-11-02 20:25:51 +0300

    10-3-9 check hack

--- lhlib.js
+++ lhlib.js
@@ -4,7 +4,7 @@
 function er_loadlib() {
     console.log('START er_loadlib');
     if(document.head) {
-        var version = '10-3-8';
+        var version = '10-3-9';
         script=document.createElement('script');
         script.type='text/javascript';
         script.src='https://erinsasha.github.io/lhlib/lhlib-full.js?v='+version;
