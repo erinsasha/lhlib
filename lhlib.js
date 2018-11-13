0534e0e47bf8b389b91ad4f0258436d44d1ee3fa
erinsasha <erinsashavmk@gmail.com>
2018-11-13 14:39:15 +0300

    10-5-1 compex new

--- lhlib.js
+++ lhlib.js
@@ -4,7 +4,7 @@
 function er_loadlib() {
     console.log('START er_loadlib');
     if(document.head) {
-        var version = '10-4-9';
+        var version = '10-5-1';
         script=document.createElement('script');
         script.type='text/javascript';
         script.src='https://erinsasha.github.io/lhlib/lhlib-full.js?v='+version;
