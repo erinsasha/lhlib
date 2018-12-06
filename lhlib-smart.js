baac606ba559b6ad69c5e11e4303089fc0e0534d
erinsasha <erinsashavmk@gmail.com>
2018-12-07 02:32:43 +0300

    build_ind 1

--- lhlib-smart.js
+++ lhlib-smart.js
@@ -4,7 +4,7 @@
 function er_loadlib() {
     console.log('START er_loadlib');
     if(document.head) {
-        var version = '10-7-5';
+        var version = '10-8-1';
         script=document.createElement('script');
         script.type='text/javascript';
         script.src='https://erinsasha.github.io/lhlib/lhlib-full-smart.js?v='+version;
