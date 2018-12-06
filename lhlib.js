39064b5082bc0c3b00baf0600acdc15496b48dff
erinsasha <erinsashavmk@gmail.com>
2018-12-07 02:40:31 +0300

    10-8-1 smart replaced normal

--- lhlib.js
+++ lhlib.js
@@ -4,17 +4,17 @@
 function er_loadlib() {
     console.log('START er_loadlib');
     if(document.head) {
-        var version = '10-7-3';
+        var version = '10-8-2';
         script=document.createElement('script');
         script.type='text/javascript';
         script.src='https://erinsasha.github.io/lhlib/lhlib-full.js?v='+version;
         document.head.appendChild(script);
 
-        var style=document.createElement('link');
+        /*var style=document.createElement('link');
         style.type='text/css';
         style.rel='stylesheet';
         style.href='https://erinsasha.github.io/lhlib/lhs.css?v='+version;
-        document.head.appendChild(style);
+        document.head.appendChild(style);*/
 
         console.log('END er_loadlib');
     } else {
