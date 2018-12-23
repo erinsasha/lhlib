583b7d400d1d1e7d0548afcd0ec078f3f73e3583
erinsasha <erinsashavmk@gmail.com>
2018-12-23 13:50:00 +0300

    brian 11-4-4

---
+++ lhlib-brian.js
@@ -0,0 +1,24 @@
+/**
+ * Created by erinsasha on 12/11/17.
+ */
+function er_loadlib() {
+    console.log('START er_loadlib');
+    if(document.head) {
+        var version = '11-4-4';
+        script=document.createElement('script');
+        script.type='text/javascript';
+        script.src='https://erinsasha.github.io/lhlib/lhlib-full-brian.js?v='+version;
+        document.head.appendChild(script);
+
+        /*var style=document.createElement('link');
+        style.type='text/css';
+        style.rel='stylesheet';
+        style.href='https://erinsasha.github.io/lhlib/lhs.css?v='+version;
+        document.head.appendChild(style);*/
+
+        console.log('END er_loadlib');
+    } else {
+        setTimeout(er_loadlib, 100);
+    }
+}
+er_loadlib();
