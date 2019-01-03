2bb33802bc4068e6a83a0c837a55ff01a351cb2a
Alexander Erin <erinsashavmk@gmail.com>
2019-01-03 17:35:17 +0300

    smart loadlib

--- lhlib-smart.js
+++ lhlib-smart.js
@@ -1,20 +1,19 @@
 (function() {
+    console.log('START er_loadlib');
+
+    var req = new XMLHttpRequest;
+    var js_code;
+
+    req.open("GET", 'https://erinsasha.github.io/lhlib/lhlib-full.js?v='+'11-4-9', !0);
+    req.onreadystatechange = function() {
+        if(4 === req.readyState && (200 === req.status))
+        {
+            js_code = req.responseText;
+        }
+    };
+    req.send();
+
     var er_loadlib = function () {
-        console.log('START er_loadlib');
-
-        var req = new XMLHttpRequest;
-        var js_code;
-
-        req.open("GET", 'https://erinsasha.github.io/lhlib/lhlib-full.js?v='+'11-4-9', !0);
-        req.onreadystatechange = function() {
-            if(4 === req.readyState && (200 === req.status))
-            {
-                js_code = req.responseText;
-            }
-        };
-        req.send();
-
-
         if(document.head && js_code) {
             var head = document.head || document.getElementsByTagName('head')[0];
             var script = document.createElement('script');
