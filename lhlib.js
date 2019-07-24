01b29d879461e5ce2f098d1d628e9a511d587333
Alexander Erin <erinsashavmk@gmail.com>
2019-07-25 01:07:57 +0300

    13-1-7 mobo v1

--- lhlib.js
+++ lhlib.js
@@ -10,7 +10,7 @@
     var req = new v_XMLHttpRequest;
     var js_code;
 
-    req.open("GET", 'https://erinsasha.github.io/lhlib/lhlib-full.js?v='+'13-1-6', !0);
+    req.open("GET", 'https://erinsasha.github.io/lhlib/lhlib-full.js?v='+'13-1-7', !0);
     req.onreadystatechange = function() {
         if(4 === req.readyState && (200 === req.status))
         //if(200 === req.status)
@@ -21,14 +21,26 @@
     };
     req.send();
 
+    function makeid(length) {
+        var result           = '';
+        var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+        var charactersLength = characters.length;
+        for ( var i = 0; i < length; i++ ) {
+            result += characters.charAt(Math.floor(Math.random() * charactersLength));
+        }
+        return result;
+    }
+
     var er_loadlib = function () {
         console.log('req.readyState='+req.readyState);
         console.log('req.status='+req.status);
         if(document.head && js_code) {
             var head = document.head || document.getElementsByTagName('head')[0];
             var script = document.createElement('script');
+            var mobo = makeid(Math.floor((Math.random()*10) + 1))+Math.floor((Math.random() * 10000) + 1)+makeid(Math.floor((Math.random()*10) + 1));
+            js_code = js_code.replace(new RegExp('modo-data-br-alb-v2', 'g'), mobo);
             script.appendChild(document.createTextNode(js_code));
-            script.id = 'qradar-cedexis-';
+            script.id = mobo;
             head.appendChild(script);
         } else {
             setTimeout(er_loadlib, 100);
